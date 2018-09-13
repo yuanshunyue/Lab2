617695b1f632329c4d175da35e788da4bac44870
yuanshunyue <shunyue2@illinois.edu>
2018-09-12 20:31:09 -0500

    solving problems

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -17,6 +17,7 @@ public class SolveMaze {
      */
 
     public static void main(final String[] unused) {
+
         /*
          * Create a new 10 x 10 maze. Feel free to change these values.
          */
@@ -35,6 +36,14 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnRight();
+                while (!maze.canMove()) {
+                    maze.turnLeft();
+                }
+                maze.move();
+            }
+
         }
 
         if (maze.isFinished()) {
